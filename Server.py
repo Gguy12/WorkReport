e30e6218ddf5f12e3d9768b9f622838ea684f0df
Gguy12 <guy1shaul@gmail.com>
2023-10-08 13:05:04 +0300

    added the db file

--- Server.py
+++ Server.py
@@ -1,1 +1,7 @@
-print()
+import socket
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from hashlib import sha256_hash
+
+print("working")
